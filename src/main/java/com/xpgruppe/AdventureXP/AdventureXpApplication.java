7c5acadde272086c864d9f43740871ae7ed4e4b4
gidgud <jonathanbirkstrom@gmail.com>
2026-02-24 14:21:14 +0100

    Initial commit

---
+++ src/main/java/com/xpgruppe/AdventureXP/AdventureXpApplication.java
@@ -0,0 +1,13 @@
+package com.xpgruppe.AdventureXP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureXpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureXpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xpgruppe/AdventureXP/AdventureXpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpgruppe.AdventureXP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureXpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
